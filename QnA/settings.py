62ef7fa44b4b30ab818088b2aec83cf691c2529e
Vedant Lahane <vedantanillahane@gmail.com>
2025-09-03 12:21:35 +0530

    fix: correct model class name and update settings for new apps

--- QnA/settings.py
+++ QnA/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qna_app',
+    'data_app',
 ]
 
 MIDDLEWARE = [
--- qna_app/models.py
+++ qna_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class UploadedFile(models.MOdel):
+class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
