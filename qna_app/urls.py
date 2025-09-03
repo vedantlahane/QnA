b9d4325e82e1ac241e4c6b25ee8562f82fc8cbcc
Vedant Lahane <vedantanillahane@gmail.com>
2025-09-03 12:28:25 +0530

    fix: update upload path to reference the correct view function

--- qna_app/urls.py
+++ qna_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Add this line
-    path('upload/', views.upload, name='upload'),
+    path('upload/', views.upload_file, name='upload'),
     path('chat/', views.chat, name='chat'),
 ]
