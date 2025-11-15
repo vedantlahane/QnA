af1c295b5149445222c6f062e36b819cd9c5aa8b
Vedant Lahane <vedantanillahane@gmail.com>
2025-11-16 01:53:45 +0530

    feat: add redirect for API endpoint to ensure proper URL structure

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,11 +3,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from .views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('api', RedirectView.as_view(url='/api/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/', include('agent.urls')),
 ]
